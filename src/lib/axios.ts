1b72574af0fbcc44985976a563c04b979e36bdb8
Eyepatch5263 <pratyushpragyey@gmail.com>
2024-12-17 23:19:53 +0530

    final changes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://spotify-backend-eye-abcd393e34b5.herokuapp.com/api",
+    baseURL: "https://spotify-backend-eye-970634887ce3.herokuapp.com/api",
 })
